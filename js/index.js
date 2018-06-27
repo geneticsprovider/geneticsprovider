baa4f6c1b0ad630a85c1ae281324019cf97c9b6a
geneticsprovider <40611261+geneticsprovider@users.noreply.github.com>
2018-06-26 17:06:06 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,14 +1,28 @@
+var Providers = new Array();
+var submitButton = document.getElementById('submitButton');
+
 $(document).ready(function(){
-  var url = "file:///Users/mcheng/Desktop/genetics-provider/ziptoprovidercopy.json";
+   var url = "provider-data.json";
   $.getJSON(url, function(data){
       console.log("JSON read");
-  })
+      Providers = data;
+  });
+  
 });
 
-
-
-function doSomethingWithTextBox()
+function getZipFromBox()
 {
-  var textBox = document.getElementById('TEXTBOX_ID');
-  
+  var queryZip = document.getElementById('textBox').value;
+  if(queryZip > 96162 || queryZip < 90001){
+    $("#prov1").html("Invalid ZIP");
+  }
+  else{
+    console.log("Zip read: " + queryZip);
+    var providerNum1 = Providers[queryZip].S1match;
+    var providerNum2 = Providers[queryZip].S2match;
+    var providerNum3 = Providers[queryZip].S3match;
+    console.log("Provider numbers found: " + providerNum1 + ", " + providerNum2 + ", " + providerNum3);
+    //document.getElementById('prov1').innerHTML = providerNum;
+    $("#prov1").html("Closest providers: <br>" + Providers[providerNum1].primary_city + "<br>" + Providers[providerNum2].primary_city + "<br>" + Providers[providerNum3].primary_city);
+  }
 }
