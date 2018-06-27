0a88d0e12b117c23bd0a9633b80e59ff6735ef46
geneticsprovider <40611261+geneticsprovider@users.noreply.github.com>
2018-06-26 18:57:00 -0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 var Providers = new Array();
-var submitButton = document.getElementById('submitButton');
 
 $(document).ready(function(){
    var url = "provider-data.json";
@@ -14,7 +13,10 @@ function getZipFromBox()
 {
   var queryZip = document.getElementById('textBox').value;
   if(queryZip > 96162 || queryZip < 90001){
-    $("#prov1").html("Invalid ZIP");
+    $("#providers").html("Invalid ZIP");
+  }
+  else if(queryZip.length == 0){
+    $("#providers").html("Enter your ZIP code to find the location and contact information of the three nearest genetics providers.");
   }
   else{
     console.log("Zip read: " + queryZip);
@@ -22,7 +24,6 @@ function getZipFromBox()
     var providerNum2 = Providers[queryZip].S2match;
     var providerNum3 = Providers[queryZip].S3match;
     console.log("Provider numbers found: " + providerNum1 + ", " + providerNum2 + ", " + providerNum3);
-    //document.getElementById('prov1').innerHTML = providerNum;
-    $("#prov1").html("Closest providers: <br>" + Providers[providerNum1].primary_city + "<br>" + Providers[providerNum2].primary_city + "<br>" + Providers[providerNum3].primary_city);
+    $("#providers").html("Closest providers: <br>" + Providers[providerNum1].primary_city + "<br>" + Providers[providerNum2].primary_city + "<br>" + Providers[providerNum3].primary_city);
   }
 }
